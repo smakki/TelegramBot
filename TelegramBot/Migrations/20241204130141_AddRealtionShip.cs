1717436474e4c5c7ad37dbaba17e337faef7e61f
belan <smakki.92@gmail.com>
2024-12-04 17:19:56 +0300

    work in progress

---
+++ TelegramBot/Migrations/20241204130141_AddRealtionShip.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TelegramBot.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRealtionShip : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "UserId",
+                table: "UserTasks",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserTasks_UserId",
+                table: "UserTasks",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserTasks_Users_UserId",
+                table: "UserTasks",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserTasks_Users_UserId",
+                table: "UserTasks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserTasks_UserId",
+                table: "UserTasks");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "UserTasks");
+        }
+    }
+}
